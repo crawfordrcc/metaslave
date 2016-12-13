52e48ca25e1be9e60034b84f981bb22a5af25d7a
Robert Crawford <rcrawford8@mail.csuchico.edu>
2016-12-13 11:05:04 -0800

    added stuff and broke stuff

--- finalproject/finalproject/metaslave/static/js/metaslave.js
+++ finalproject/finalproject/metaslave/static/js/metaslave.js
@@ -3,13 +3,14 @@ const AKEY=65;
 const SKEY=83;
 const DKEY=68;
 const EKEY=69;
+const XKEY=88;
 var player;
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 var width = canvas.getAttribute('width');
 var height = canvas.getAttribute('height');
 var left = canvas.offsetLeft;
-var currentTileMap
+var currentGameMap;
 tileSize = 64;
 cols = 16;
 rows = 12;
@@ -33,16 +34,48 @@ function tilemap(visual, objects){
   //this.rows = 16;
   this.tiles = new Image();
   this.objects = new Image();
+  this.npcs = new Image();
   this.tiles.src = "/static/images/tiles.png";
   this.objects.src = "/static/images/building.png";
+  this.numObjectTiles = 9;
+  this.npcs.src = "/static/images/npcs.png";
   this.visualGrid = visual;
   this.objectGrid = objects;
   this.getTile = function(row,col){
     return this.visualGrid[row*cols + col]
   }
   this.getObjectTile = function(row, col){
-    return this.objectGrid[row*cols + col]
+    var tile = new Array();
+    tile = []
+    tile[0] = this.objectGrid[row*cols + col]
+    if(tile[0] > this.numObjectTiles){
+      tile[1] = this.npcs
+      tile[0] -= this.numObjectTiles
+    }
+    else
+      tile[1] = this.objects
+    return tile;
   }
+
+  this.addObject = function(object){
+    if(object.length == undefined)
+      this.objectGrid[object.row*cols + object.col] = object.id;
+    else{
+      for(var i = 0; i < object.length; ++i){
+        this.objectGrid[object[i].row*cols + object[i].col] = object[i].id;
+      }
+    }
+  }
+
+  this.removeObject = function(object){
+    if(object.length == undefined)
+      this.objectGrid[object.row*cols + object.col] = 0;
+    else{
+      for(var i = 0; i < object.length; ++i)
+        this.objectGrid[object[i].row*cols + object[i].col] = 0;
+    }
+  }
+
   this.drawTileMap = function(){
     for (var c = 0; c < cols; c++) {
         for (var r = 0; r < rows; r++) {
@@ -59,10 +92,10 @@ function tilemap(visual, objects){
                 tileSize,
                 tileSize
             );
-            if(object > 0)
+            if(object[0] > 0)
               ctx.drawImage(
-                this.objects,
-                (object-1) * tileSize,
+                object[1],
+                (object[0]-1) * tileSize,
                 0,
                 tileSize,
                 tileSize,
@@ -74,6 +107,7 @@ function tilemap(visual, objects){
         }
     }
   }
+
   this.updateTiles = function(playerRow, playerCol, direction){
     tile = this.getTile(playerRow, playerCol)
     ctx.drawImage(
@@ -101,10 +135,10 @@ function tilemap(visual, objects){
           tileSize
       );
       object = this.getObjectTile(playerRow - 1, playerCol)
-      if(object > 0){
+      if(object[0] > 0){
         ctx.drawImage(
-            this.objects,
-            (object-1) * tileSize,
+            object[1],
+            (object[0]-1) * tileSize,
             0,
             tileSize,
             tileSize,
@@ -153,10 +187,10 @@ function tilemap(visual, objects){
             tileSize
           );
           object = this.getObjectTile(r, c)
-          if(object > 0){
+          if(object[0] > 0){
             ctx.drawImage(
-              this.objects,
-              (object-1) * tileSize,
+              object[1],
+              (object[0]-1) * tileSize,
               0,
               tileSize,
               tileSize,
@@ -170,6 +204,37 @@ function tilemap(visual, objects){
     }
   }
 };
+
+function gameMap(tilemap, npcs, left, right, down, up){
+  this.tilemap = tilemap;
+  this.npcs = npcs;
+  if(this.npcs)
+    this.tilemap.addObject(npcs);
+  this.nextMap = [];
+  this.nextMap[0] = right;
+  this.nextMap[1] = left;
+  this.nextMap[2] = up;
+  this.nextMap[3] = down;
+  this.actionObject = npcs[0];
+
+  this.drawTileMap = function() {
+    tilemap.drawTileMap();
+  }
+
+  this.update = function(row, col, direction){
+    tilemap.updateTiles(row, col, direction);
+    for(var i = 0; i < this.npcs.length; ++i)
+      this.npcs[i].update();
+  }
+
+  this.getObject = function(row, col){
+    return tilemap.getObjectTile(row, col);
+  }
+
+  this.getTile = function(row, col){
+    return tilemap.getTile(row, col);
+  }
+}
 
 
 visual1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
@@ -196,7 +261,7 @@ object1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0,
            0, 7, 8, 8, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
            0, 4, 5, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
            0, 4, 5, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-           0, 1, 2, 2, 2, 3, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+           0, 1, 2, 2, 2, 3, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 var map1 = new tilemap(visual1, object1)
 
@@ -220,15 +285,21 @@ function mainMenu(){
   var mouseX, mouseY;
   canvas.addEventListener("mouseup", function(e){
       var mouse = getMousePosition(e);
-      if(mouse.x > rect[0] && mouse.x < rect[0] + rect[2] && mouse.y > rect[1] && mouse.y < rect[1]+rect[3])
+      if(mouse.x > rect[0] && mouse.x < rect[0] + rect[2] && mouse.y > rect[1] && mouse.y < rect[1]+rect[3]){
+          removeEventListener("mouseup", this);
           gamestart();
+      }
   });
 }
 
 function gamestart(){
   player = new component(64, 64, "/static/images/new_robot.png", 128, 128);
-  currentTileMap = map1;
-  currentTileMap.drawTileMap();
+  var npcs = new Array()
+  npcs[0] = new npc(6, 7, 11, ['Hello!', 'My name is Paul.']);
+  //npcs[1] = new npc(10, 6, 13, ['What is the meaning of life?']);
+  currentGameMap = new gameMap(map1, npcs)
+  currentGameMap.drawTileMap();
+  player.update();
   gameArea.start();
 }
 
@@ -236,22 +307,152 @@ var gameArea = {
 
     start : function() {
         this.frameNo = 0;
-        window.addEventListener('keydown', function (e) {
-           gameArea.keys = (gameArea.keys || []);
-           gameArea.keys[e.keyCode] = true;
-       })
-       window.addEventListener('keyup', function (e) {
-           gameArea.keys[e.keyCode] = false;
-       })
+        this.keys = [];
+        this.lastKey = new Array();
+        window.addEventListener('keydown', onKeydown);
+        window.addEventListener('keyup', onKeyup);
         this.interval = setInterval(updateGameArea, 20);
-        },
+    },
     clear : function() {
         ctx.clearRect(0, 0, width, height);
     },
     stop : function() {
+        window.removeEventListener('keydown', onKeydown);
+        window.removeEventListener('keyup', onKeyup);
         clearInterval(this.interval);
     }
 
+}
+
+function onKeydown(e) {
+  key = e.keyCode;
+  if(key == EKEY){
+      gameArea.stop();
+      playerMenu.start();
+  }
+/*  if(key == XKEY){
+    if(currentGameMap.actionObject.nextToPlayer()){
+      gameArea.stop();
+      currentGameMap.actionObject.action();
+    }
+  }*/
+  if(!gameArea.keys[key]){
+    gameArea.keys[key] = true;
+    gameArea.lastKey.unshift(key);
+  }
+
+  /*
+  if(player.moving == 0){
+      if(key == WKEY){
+          player.moving = 4;
+          //player.wobbleIncrement *= -1;
+        }
+        if(key == AKEY){
+          player.moving = 2;
+          //player.wobbleIncrement *= -1;
+        }
+        if(key == SKEY){
+          player.moving = 3;
+        }
+        if(key == DKEY){
+          player.moving = 1;
+        }
+    }*/
+}
+
+function onKeyup(e) {
+  key = e.keyCode;
+  gameArea.keys[key] = false;
+  for(var i = 0; i < gameArea.lastKey.length; ++i){
+    if(gameArea.lastKey[i] == key)
+      gameArea.lastKey.splice(i, 1);
+  }
+  /*
+    if(player.moving > 0){
+      key = e.keyCode;
+      if(key == WKEY || key == SKEY || key == AKEY || key == DKEY)
+        player.moving = 0;
+    }*/
+}
+
+function npc(row, col, id, text) {
+    this.row = row;
+    this.col = col;
+    this.id = id;
+    this.text = text;
+    this.talk = false;
+    //this.talkFunction = talkFunction;
+
+    this.update = function() {
+        this.talk = this.nextToPlayer();
+        if(this.talk)
+          currentGameMap.actionObject = this;
+    }
+
+    this.nextToPlayer = function() {
+        if(this.row == player.tiley && Math.abs(this.col - player.tilex) == 1)
+            return true;
+        else if(this.col == player.tilex && Math.abs(this.row - player.tiley) == 1)
+            return true;
+        else
+            return false;
+    }
+
+    this.action = function() {
+      conversation.start();
+      conversation.text = this.text;
+      conversation.len = this.text.length;
+    }
+}
+
+var conversation = {
+  start : function() {
+      this.frameNo = 0;
+      this.index = 0;
+      this.text;
+      this.len;
+      this.statement = 0;
+      this.next = false;
+      this.keys = []
+      this.done = false;
+      window.addEventListener('keydown', function (e) {
+         conversation.keys[e.keyCode] = true;
+     })
+     window.addEventListener('keyup', function (e) {
+         conversation.keys[e.keyCode] = false;
+     })
+      this.interval = setInterval(updateConversation, 20);
+      },
+  clear : function() {
+      ctx.clearRect(0, 0, width, height);
+  },
+  stop : function() {
+      clearInterval(this.interval);
+  }
+}
+
+function updateConversation(){
+    if(conversation.next && conversation.keys[XKEY]){
+      if(++conversation.statement < conversation.len){
+        conversation.next = false;
+        conversation.index = 0;
+      }
+      else {
+        conversation.done = true;
+        conversation.stop()
+        gameArea.start()
+      }
+    }
+    if(conversation.frameNo == 0 && !conversation.done){
+      if(conversation.index < conversation.text[conversation.statement].length){
+        console.log(conversation.text[conversation.statement].charAt(conversation.index++));
+      }
+      else if(!conversation.next){
+        conversation.next = true;
+        console.log('');
+      }
+    }
+    conversation.frameNo = ++conversation.frameNo%2;
 }
 
 function component(width, height, filename, x, y) {
@@ -324,62 +525,54 @@ function staticComponent(){
 }
 
 function updateGameArea() {
-  if(gameArea.keys){
-    if(gameArea.keys[EKEY]){
-      saveGame();
+  var object;
+  if(player.nextmove){
+    player.nextmove = false;
+    player.moving = 0;
+  }
+
+  if(gameArea.keys && player.moving == 0){
+    if(gameArea.keys[DKEY] && gameArea.lastKey[0] == DKEY){
+      var object;
+      if(player.tilex == (cols - 1))
+        object = currentGameMap.getObject(player.tiley, player.tilex) + 1
+      else
+        object = currentGameMap.getObject(player.tiley, player.tilex + 1)
+      if(object[0] <= 0)
+        player.moving = 1
+    }
+    else if(gameArea.keys[AKEY] && gameArea.lastKey[0] == AKEY){
+      var object;
+      if(player.tilex == 0)
+        object = currentGameMap.getObject(player.tiley, player.tilex) + 1
+      else
+        object = currentGameMap.getObject(player.tiley, player.tilex - 1)
+      if(object[0] <= 0)
+        player.moving = 2
+    }
+    else if(gameArea.keys[SKEY] && gameArea.lastKey[0] == SKEY){
+      var object;
+      if(player.tiley == (rows - 1))
+        object = currentGameMap.getObject(player.tiley, player.tilex) + 1
+      else
+        object = currentGameMap.getObject(player.tiley  + 1, player.tilex)
+      if(object[0] <= 0)
+        player.moving = 3
+    }
+    else if(gameArea.keys[WKEY] && gameArea.lastKey[0] == WKEY){
+      if(player.tiley == 0)
+        object = currentGameMap.getObject(player.tiley, player.tilex) + 1
+      else
+        object = currentGameMap.getObject(player.tiley - 1, player.tilex)
+      if(object[0] <= 0)
+        player.moving = 4;
     }
   }
-    if(player.nextmove){
-      player.nextmove = false;
-      player.moving = 0;
-    }
-    if(player.moving == 0){
-      if(gameArea.keys){
-        if(gameArea.keys[WKEY]){
-          var object;
-          if(player.tiley == 0)
-            object = currentTileMap.getObjectTile(player.tiley, player.tilex) + 1
-          else
-            object = currentTileMap.getObjectTile(player.tiley - 1, player.tilex)
-          if(object <= 0)
-            player.moving = 4
-          //player.wobbleIncrement *= -1;
-        }
-        if(gameArea.keys[AKEY]){
-          var object;
-          if(player.tilex == 0)
-            object = currentTileMap.getObjectTile(player.tiley, player.tilex) + 1
-          else
-            object = currentTileMap.getObjectTile(player.tiley, player.tilex - 1)
-          if(object <= 0)
-            player.moving = 2
-          //player.wobbleIncrement *= -1;
-        }
-        if(gameArea.keys[SKEY]){
-          var object;
-          if(player.tiley == (rows - 1))
-            object = currentTileMap.getObjectTile(player.tiley, player.tilex) + 1
-          else
-            object = currentTileMap.getObjectTile(player.tiley  + 1, player.tilex)
-          if(object <= 0)
-            player.moving = 3
-        }
-        if(gameArea.keys[DKEY]){
-          var object;
-          if(player.tilex == (cols - 1))
-            object = currentTileMap.getObjectTile(player.tiley, player.tilex) + 1
-          else
-            object = currentTileMap.getObjectTile(player.tiley, player.tilex + 1)
-          if(object <= 0)
-            player.moving = 1
-
-        }
-      }
-    }
-
-    currentTileMap.updateTiles(player.tiley, player.tilex, player.moving);
+  if(player.moving > 0){
+    currentGameMap.update(player.tiley, player.tilex, player.moving);
     player.newPos();
     player.update();
+  }
 }
 
 function getMousePosition(e){
@@ -393,7 +586,25 @@ function getMousePosition(e){
   }
 }
 
-
+var playerMenu = {
+  start : function() {
+      this.frameNo = 0;
+      window.addEventListener('keydown', function (e) {
+         gameArea.keys = (gameArea.keys || []);
+         gameArea.keys[e.keyCode] = true;
+     })
+     window.addEventListener('keyup', function (e) {
+         gameArea.keys[e.keyCode] = false;
+     })
+      this.interval = setInterval(updateGameArea, 20);
+      },
+  clear : function() {
+      ctx.clearRect(0, 0, width, height);
+  },
+  stop : function() {
+      clearInterval(this.interval);
+  }
+}
 
 function saveGame(){
     $.ajax({
@@ -404,26 +615,9 @@ function saveGame(){
             name:'bob',
             x_position: player.x.toString(),
             y_position: player.y.toString(),
-            currentTileMap: 'map1'
+            currentGameMap: 'map1'
 
         },
         success:function(){}
       });
-
-
-/*$.post('game/save',
-{
-    name : 'bob', //pass the score here
-    x_position: player.x,  // pass the win value here
-    y_position: player.y,
-    currentTileMap: 'map1'
-},
-function(data) {
-    if(data.status == 1){
-        // success! Do something
-    }
-    else{
-        // error! Do something
-    }
-});*/
 }
